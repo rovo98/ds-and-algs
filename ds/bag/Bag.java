87a7587a0721b1e4d29e919b09616464f0de7b7f
rovo98 <2402323937@qq.com>
2018-03-04 14:40:28 +0800

    Create Bag.java

---
+++ ds/bag/Bag.java
@@ -0,0 +1,20 @@
+package com.rovo98.ds.bag.datastructures;
+
+/**
+ * ADT Bag
+ *
+ * Data
+ *
+ * Operation
+ *      boolean isEmpty()       Returns true if bag is empty, otherwise false.
+ *      int size()              Returns the number of items in bag.
+ *      void add(Item item)     Add an item into bag.
+ * endADT
+ * @author rovo98
+ * Date: 3/2/2018
+ */
+public interface Bag<Item> extends Iterable<Item> {
+    boolean isEmpty();
+    int size();
+    void add(Item item);
+}
