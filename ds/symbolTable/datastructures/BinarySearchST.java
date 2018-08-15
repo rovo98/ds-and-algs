a9b8dab8be3ebd3951a8bb198e1eb36bfe0541b1
rovo98 <2402323937@qq.com>
2018-08-15 20:47:13 +0800

    reformat BinarySearchST.java

--- ds/symbolTable/datastructures/BinarySearchST.java
+++ ds/symbolTable/datastructures/BinarySearchST.java
@@ -7,19 +7,20 @@ import java.util.NoSuchElementException;
  * ADT BinarySearchST, an implementation of ST(Symbol Table) using resizing arrays and binarySearch algorithm.
  *
  * Data:
- *          Key[] keys
- *          Value[] values
- *          int n                           The number of keys in symbol table.
+ * Key[] keys
+ * Value[] values
+ * int n                           The number of keys in symbol table.
  * Operations:
- *          BinarySearchST()                Initializes an empty symbol table.
- *          boolean isEmpty()               Returns true if symbol table is empty, otherwise false.
- *          boolean contains(Key key)       Returns true if {@code key} in symbol table, {@code false} otherwise.
- *          int size()                      Returns the number of keys in symbol table.
- *          void put(Key key, Value value)  Add a key-value into symbol table, update value of a key-value if {@code key} already in table.
- *          void delete(Key key)            Remove a key-value from symbol table.
- *          Value get(key key)              Get the value of a key-value which key is {@code key}, returns null if {@code key} not in table.
- *          Iterable<Key> keys()            Returns an iterable obj that iterates over keys in symbol table.
+ * BinarySearchST()                Initializes an empty symbol table.
+ * boolean isEmpty()               Returns true if symbol table is empty, otherwise false.
+ * boolean contains(Key key)       Returns true if {@code key} in symbol table, {@code false} otherwise.
+ * int size()                      Returns the number of keys in symbol table.
+ * void put(Key key, Value value)  Add a key-value into symbol table, update value of a key-value if {@code key} already in table.
+ * void delete(Key key)            Remove a key-value from symbol table.
+ * Value get(key key)              Get the value of a key-value which key is {@code key}, returns null if {@code key} not in table.
+ * Iterable<Key> keys()            Returns an iterable obj that iterates over keys in symbol table.
  * endADT
+ *
  * @author rovo98
  * Date: 2018.3.25
  *
@@ -36,12 +37,13 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
     @SuppressWarnings("unchecked")
     public BinarySearchST() {
         keys = (Key[]) new Comparable[1];
-        values= (Value[]) new Object[1];
+        values = (Value[]) new Object[1];
         n = 0;
     }
 
     /**
      * Resizing the keys and values arrays.
+     *
      * @param max the new size of the resizing arrays.
      */
     @SuppressWarnings("unchecked")
@@ -58,9 +60,10 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
 
     /**
      * Returns the value of the {@code key}
+     *
      * @param key the key to be searched.
-     * @return  the value of the {@code key} if key in symbol table;
-     *          {@code null} otherwise.
+     * @return the value of the {@code key} if key in symbol table;
+     * {@code null} otherwise.
      */
     /*
     Time complexity: O(log n).
@@ -76,7 +79,8 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
 
     /**
      * Add a key-value into symbol table
-     * @param key the key of key-value to add to symbol table;
+     *
+     * @param key   the key of key-value to add to symbol table;
      * @param value the value of key-value to add to symbol table.
      */
     /*
@@ -102,16 +106,17 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
         // use a idea of insertion sort the insert the key to keys[0, .. n-1]
         int j = n - 1;
         for (; j >= 0 && key.compareTo(keys[j]) < 0; j--) {
-            keys[j+1] = keys[j];
-            values[j+1] = values[j];
+            keys[j + 1] = keys[j];
+            values[j + 1] = values[j];
         }
-        keys[j+1] = key;
-        values[j+1] = value;
+        keys[j + 1] = key;
+        values[j + 1] = value;
         n++;
     }
 
     /**
      * Remove a key-value from symbol table.
+     *
      * @param key the key of a key-value to remove.
      * @throws NoSuchElementException if {@code key} not in symbol table.
      */
@@ -125,19 +130,21 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
         keys[index] = null;
         values[index] = null;
         for (int i = index; i < n; i++) {
-            keys[i] = keys[i+1];
-            values[i] = values[i+1];
+            keys[i] = keys[i + 1];
+            values[i] = values[i + 1];
         }
         n--;
         if (n == keys.length / 4) {
             resize(keys.length / 2);
         }
     }
+
     /**
      * Returns true if {@code key} in symbol table.
+     *
      * @param key the key to be searched.
-     * @return  {@code true} if key in symbol table;
-     *          {@code false} otherwise.
+     * @return {@code true} if key in symbol table;
+     * {@code false} otherwise.
      */
     @Override
     public boolean contains(Key key) {
@@ -146,8 +153,9 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
 
     /**
      * Returns true if symbol table is empty
+     *
      * @return {@code true} if symbol table if empty;
-     *          {@code false} otherwise.
+     * {@code false} otherwise.
      */
     @Override
     public boolean isEmpty() {
@@ -156,12 +164,14 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
 
     /**
      * Returns the number of keys in symbol table.
+     *
      * @return the number of keys in symbol table.
      */
     @Override
     public int size() {
         return n;
     }
+
     /**************************************
      * BinarySearch tool.
      **************************************/
@@ -174,8 +184,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
                 return mid;
             } else if (keys[mid].compareTo(key) > 0) {
                 hi = mid - 1;
-            }
-            else {
+            } else {
                 lo = mid + 1;
             }
         }
@@ -184,36 +193,47 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
 
     /**
      * Returns an iterable obj that iterates over keys in symbol table in ascending order.
+     *
      * @return an iterable obj that iterates over keys in symbol table in ascending order.
      */
     @Override
     public Iterable<Key> keys() {
         return new BinarySearchSTIterator();
     }
+
     private class BinarySearchSTIterator implements Iterable<Key> {
-            public Iterator<Key> iterator() {
-                return new STIterator();
+        @Override
+        public Iterator<Key> iterator() {
+            return new STIterator();
+        }
+
+        // A symbol table iterator that iterates over keys in table.
+        private class STIterator implements Iterator<Key> {
+            private int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < n;
             }
-            // A symbol table iterator that iterates over keys in table.
-            private class STIterator implements Iterator<Key> {
-                private int i = 0;
-                public boolean hasNext() {
-                    return i < n;
+
+            @Override
+            public Key next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
                 }
-                public Key next() {
-                    if (!hasNext()) {
-                        throw new NoSuchElementException();
-                    }
-                    return keys[i++];
-                }
-                public void remove() {
-                    throw new UnsupportedOperationException();
-                }
+                return keys[i++];
             }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        }
     }
 
     /**
      * Unit tests the {@code BinarySearchST} data type.
+     *
      * @param args command arguments.
      */
     public static void main(String[] args) {
